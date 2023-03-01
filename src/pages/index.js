c57aab15dce14158293fb44e6a062b7e8c5a257b
Mohamed D. Ahmed <mohamedahmed754@hotmail.com>
2023-03-01 13:29:37 -0500

    updated resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -94,7 +94,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="Resume 2023.pdf"
+                  href="Resume2.0.pdf"
                   target={"_blank"}
                 >
                   Resume
